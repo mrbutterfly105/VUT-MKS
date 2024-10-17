662dd2c9f22b0791ed16c34a9b43d74152d2a6b1
mrbutterfly105 <102173814+mrbutterfly105@users.noreply.github.com>
2024-10-17 15:03:16 +0200

    dodělat

---
+++ cvika1/Core/Inc/sct.h
@@ -0,0 +1,19 @@
+/*
+ * sct1.h
+ *
+ *  Created on: Oct 10, 2024
+ *      Author: topsecret
+ */
+
+#ifndef INC_SCT_H_
+#define INC_SCT_H_
+
+#include "main.h"
+
+void sct_init(void);
+
+void sct_led(uint32_t value);
+
+void sct_value(uint16_t value);
+
+#endif /* INC_SCT_H_ */
