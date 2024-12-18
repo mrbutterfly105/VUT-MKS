52da54ea76b1a128e5325ba8af0a21865742fd74
mrbutterfly105 <paulyskot@gmail.com>
2024-12-18 08:27:59 +0100

    ahooooj

---
+++ cvicimsi4/Core/Inc/sct.h
@@ -0,0 +1,23 @@
+/*
+ * sct1.h
+ *
+ *  Created on: Oct 10, 2024
+ *      Author: topsecret
+ */
+
+#ifndef INC_SCT_H_
+#define INC_SCT_H_
+
+#include "main.h"
+
+void sct_init(void);
+
+void sct_led(uint32_t value);
+
+void sct_value(uint16_t value, uint8_t led, uint8_t dot);
+
+void sct_baragraph(uint8_t led);
+
+void sct_universal_baragraph(uint8_t led_binary_index);
+
+#endif /* INC_SCT_H_ */
